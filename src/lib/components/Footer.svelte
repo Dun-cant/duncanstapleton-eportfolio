bf2added95d6cd9b0317836771b049c346a76900
Duncan <duncanstapleton04@gmail.com>
2025-05-07 07:54:32 -0700

    Initial commit

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+    import  { type Content, isFilled } from '@prismicio/client';
+	import { PrismicLink } from '@prismicio/svelte';
+    import IconGithub from '~icons/fa-brands/github'
+    import IconGitlab from '~icons/fa-brands/gitlab'
+    import IconDiscord from '~icons/fa-brands/discord'
+	import Bounded from './Bounded.svelte';
+
+    // Use the Content.SettingsDocument type for the settings prop
+    export let settings: Content.SettingsDocument;
+</script>
+
+<Bounded as="footer" class = "text-slate-600">
+    <div class="container mx-auto flex mt-20 flex-col items-center justify-between gap-6 py-8 sm:flex-row">
+        <div class="name flex flex-col items-center justify-center gap-x-4 gap-y-2 sm:flex-row sm:justify-self-start">
+            <a href="/" class="text-l font-extrabold tracking-tighter text-slate-100 transition-color duration-150 hover:text-yellow-400">
+                {settings.data.name}
+            </a>
+            <span class="hidden text-5xl font-extralight leading-[0] text-slate-400 sm:inline" aria-hidden="true">/</span>
+            <p class="text-sm text-slate-300"></p>
+            © {new Date().getFullYear()} {settings.data.name}. All rights reserved.
+        </div>
+        <nav class="navigation" aria-label="Footer Navigation">
+            <ul class="flex items-center gap-1">
+                {#each settings.data.nav_item as {link, label}, index}
+                <li>
+                    <PrismicLink field={link} class="block px-3 py-1 text-base font-bold text-slate-100 transistion-colors duration-150 hover:text-yellow-400">{label}</PrismicLink>
+                </li>
+                {#if index < settings.data.nav_item.length - 1}
+                    <span class="text-4xl font-thin leading-[0]0 text-slate-400" aria-hidden="true">
+                        /
+                    </span>
+                {/if}
+                {/each}
+            </ul>
+        </nav>
+        <div class="socials inline-flex justify-center sm-justify-end">
+            {#if isFilled.link(settings.data.github_link)}
+            <PrismicLink 
+                field={settings.data.github_link} 
+                class="p-2 text-2xl text-slate-300 
+                        transform transition-all duration-150 
+                        hover:scale-125 hover:text-yellow-400"
+                aria-label={settings.data.name + ' on Github'}
+            >
+                <IconGithub />
+            </PrismicLink>
+            {/if}
+            {#if isFilled.link(settings.data.github_link)}
+            <PrismicLink 
+                field={settings.data.github_link} 
+                class="p-2 text-2xl text-slate-300 
+                        transform transition-all duration-150 
+                        hover:scale-125 hover:text-yellow-400"
+                aria-label={settings.data.name + ' on Github'}
+            >
+                <IconGitlab />
+            </PrismicLink>
+            {/if}
+            {#if isFilled.link(settings.data.github_link)}
+            <PrismicLink 
+                field={settings.data.github_link} 
+                class="p-2 text-2xl text-slate-300 
+                        transform transition-all duration-150 
+                        hover:scale-125 hover:text-yellow-400"
+                aria-label={settings.data.name + ' on Github'}
+            >
+                <IconDiscord />
+            </PrismicLink>
+            {/if}
+        </div>
+    </div>
+</Bounded>
---
+++ src/lib/slices/Hero/Shapes.svelte
@@ -0,0 +1,58 @@
+<script lang='ts'>
+    import { T as Threlte } from "@threlte/core";
+    import * as THREE from 'three';
+    import Geometry from './Geometry.svelte';
+    import { ContactShadows, Environment, interactivity, transitions } from '@threlte/extras';
+
+    interactivity();
+    transitions();
+</script>
+
+<Threlte.PerspectiveCamera
+    makeDefault
+    position={[0,0,25]}
+    aspect={1}
+    fov={30}
+    near={1}
+    far={40}
+/>
+
+<Threlte.DirectionalLight position = {[0, 10, 30]} intensity={2}></Threlte.DirectionalLight>
+<ContactShadows position={[0, -3.5, 0]} opacity={0.65} scale={40} blur={1} far={9} />
+
+<Environment files="smallroom.hdr" path="/" format="hdr" />
+
+<!-- Icosahedron -->
+<Geometry 
+    position={[0, 0, 0,]} 
+    rate={0.3} 
+    geometry={new THREE.IcosahedronGeometry(3)}
+/>
+
+<!-- Dodecahedron -->
+<Geometry 
+    position={[1, -0.75, 4]} 
+    rate={0.4} 
+    geometry={new THREE.DodecahedronGeometry(1)}
+/>
+
+<!-- Octahedron -->
+<Geometry 
+    position={[-1.4, 2, -4]} 
+    rate={0.6} 
+    geometry={new THREE.OctahedronGeometry(1)}
+/>
+
+<!-- Torus -->
+<Geometry 
+    position={[-.8, -0.75, 5]} 
+    rate={0.6} 
+    geometry={new THREE.TorusGeometry(0.6, 0.25, 16, 32)}
+/>
+
+<!-- Icosahedron -->
+<Geometry 
+    position={[1.6, 1.6, -4]} 
+    rate={0.7} 
+    geometry={new THREE.TetrahedronGeometry(1.5)}
+/>
